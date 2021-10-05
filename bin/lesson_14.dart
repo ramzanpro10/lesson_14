5ebf208d2001af8dc978f070ae48cc487e325d63
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-05 22:21:26 +0600

    УРОК

--- bin/lesson_14.dart
+++ bin/lesson_14.dart
@@ -1,3 +1,23 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  List<dynamic> mfkjj = [25, 08];
+  print(mfkjj);
+  mfkjj.add(2006);
+  mfkjj.add(2006);
+  mfkjj.add(2006);
+  //print(mfkjj);
+  //List<int> newListik = mfkjj.reversed.toList();
+  var newListik = mfkjj.reversed.toList();
+  // print(newListik);
+  // print(newListik.runtimeType);
+  // print(newListik.first);
+  // print(newListik.first.runtimeType);
+
+  // var tyty = newListik.first - 2006;
+  // print(tyty);
+  print(mfkjj);
+  //print(mfkjj.indexOf(777));
+  // var bbb = mfkjj.indexWhere((element) => element > 1000);
+  var sss = mfkjj.indexWhere((element) => element.isOdd);
+  print('Подходит элемент номер $sss');
+  print(mfkjj[sss]);
 }
