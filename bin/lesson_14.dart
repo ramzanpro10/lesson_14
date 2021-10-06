d58938ef9e716dc1ada7b2360c778c0592db0f92
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-06 19:54:29 +0600

    next

--- bin/lesson_14.dart
+++ bin/lesson_14.dart
@@ -1,23 +1,5 @@
-void main(List<String> arguments) {
-  List<dynamic> mfkjj = [25, 08];
-  print(mfkjj);
-  mfkjj.add(2006);
-  mfkjj.add(2006);
-  mfkjj.add(2006);
-  //print(mfkjj);
-  //List<int> newListik = mfkjj.reversed.toList();
-  var newListik = mfkjj.reversed.toList();
-  // print(newListik);
-  // print(newListik.runtimeType);
-  // print(newListik.first);
-  // print(newListik.first.runtimeType);
-
-  // var tyty = newListik.first - 2006;
-  // print(tyty);
-  print(mfkjj);
-  //print(mfkjj.indexOf(777));
-  // var bbb = mfkjj.indexWhere((element) => element > 1000);
-  var sss = mfkjj.indexWhere((element) => element.isEven);
-  print('Подходит элемент номер $sss');
-  print(mfkjj[sss]);
+void main() {
+  var rareno = [1, 4, 3, 8, 6];
+  rareno.insert(0, 5);
+  print(rareno);
 }
