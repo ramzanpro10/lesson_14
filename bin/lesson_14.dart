d4b837e49275ec4b517d8adaacd8416656587215
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-06 21:36:59 +0600

    конец урока 15

--- bin/lesson_14.dart
+++ bin/lesson_14.dart
@@ -1,5 +1,36 @@
+import 'package:collection/collection.dart';
+
 void main() {
-  var rareno = [1, 4, 3, 8, 6];
-  rareno.insert(0, 5);
+  var rareno = [777, 4, 3, 8, 6, 777, 999];
+  // rareno.insert(0, 5);
   print(rareno);
+  //var novSpisok = rareno;
+  var novSpisok = [777, 4, 3, 8, 6, 777];
+  // print('Hi');
+  // print(novSpisok);
+  // print(rareno.equals(novSpisok));
+  var fff = rareno.indexOf(777);
+  var ffg = rareno.lastIndexOf(777);
+  print(fff);
+  print(ffg);
+  //const myWhiteColor = Colors.yellow(600);
+
+  //rareno.remove(777);
+  //rareno.removeWhere((element) => element == 777);
+  print(rareno);
+  rareno.sort();
+  print(rareno);
+  var newList = [];
+  rareno.forEach((element) {
+    print('my element = $element');
+    element++;
+    newList.add(element);
+  });
+  print(newList);
+
+  var listik = rareno.map((element) => element + 1);
+  listik = listik.toList();
+  print(listik);
+  print(listik.runtimeType);
+  //myKartinki = ['http://google.kg/karti.jgg', 'http:/'];
 }
